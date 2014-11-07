05b84096ec25a56789fd407eb6a7b2b99782ea5a
justscu <justscu@163.com>
2014-11-07 10:24:42 +0800

    Create bigstringadd.c
    大数相加，使用字符串

---
+++ bigstringadd.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <assert.h>
+
+char* bigadd(const char* a, const char* b, char* res)
+{
+	assert(a && b && res);
+	int aLen = strlen(a);
+	int bLen = strlen(b);
+	int maxLen = (aLen>bLen)?aLen:bLen;
+	int minLen = (aLen>bLen)?bLen:aLen;
+	const char* maxStr = (aLen>bLen)?a:b;
+	const char* minStr = (aLen>bLen)?b:a;
+
+	memset(res, 0, maxLen+2);
+	int i = 0;
+	for(i = minLen-1; i >= 0; --i)
+	{
+		res[maxLen-minLen+i+1] += minStr[i] + maxStr[maxLen-minLen+i]-'0';
+		if(res[maxLen-minLen+i+1] > '9')
+		{
+			res[maxLen-minLen+i+1] -= 10;
+			res[maxLen-minLen+i] = 1;
+		}
+	}
+
+	for(i = maxLen-minLen-1; i >= 0; --i)
+	{
+		res[i+1] += maxStr[i];
+		if(res[i+1] > '9')
+		{
+			res[i+1] -= 10;
+			res[i] = 1;
+		}
+	}
+
+	if(res[0] == 0)
+		return res+1;
+	else
+	{
+		res[0] += '0';
+		return res;
+	}
+}
